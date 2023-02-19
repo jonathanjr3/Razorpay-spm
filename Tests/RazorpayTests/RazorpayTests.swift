2f4ed5ba039f1af104e65675ee70a87ae18bad7f
jonathanjr3 <jonathan.rajya@decathlon.com>
2023-02-19 10:58:26 +0530

    Removed unnecessary code

--- Tests/RazorpayTests/RazorpayTests.swift
+++ Tests/RazorpayTests/RazorpayTests.swift
@@ -1,11 +1,1 @@
-import XCTest
-@testable import Razorpay
 
-final class RazorpayTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Razorpay().text, "Hello, World!")
-    }
-}
