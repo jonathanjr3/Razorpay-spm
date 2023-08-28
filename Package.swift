2fff2142584a51c088b9936069334adabd0986c5
jonathanjr3 <jonathan.rajya@decathlon.com>
2023-08-28 10:24:55 +0530

    Updated version to 1.3.4

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "Razorpay",
-                      url: "https://github.com/razorpay/razorpay-pod/releases/download/1.3.3/Razorpay.xcframework.zip",
-                      checksum: "c13f43ffc1cfda092ef59d9471bd3dfeb33bf215a13cae0c7adbf46c9abc0614")
+                      url: "https://github.com/razorpay/razorpay-pod/releases/download/1.3.4/Razorpay.xcframework.zip",
+                      checksum: "7a9aee4f56d1ff0bbedc19451b0c01f26640cb9b4b043502b6e8b3d1e1e3bbbe")
     ]
 )
